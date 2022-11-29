39afdd88e19efedd249a657161f1bc378abdcf44
Andrew Lam <andrewlam2193@gmail.com>
2022-11-28 19:46:56 -0800

    Best Practices

---
+++ hangman.asm
@@ -0,0 +1,4 @@
+# Assignment: MIPS Assembly Final Project 
+# Course: CS2640.01
+# Group: Andrew Lam, Tyler Hipolito, Ben Le, James Contreras 
+# Objective: Create a game of Hangman in MIPS assembly langauge. Utilize all that we have learned in CS2640. 
