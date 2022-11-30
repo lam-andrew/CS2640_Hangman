02a9e3003f139ed062eba2845ff57135858b6c4c
Andrew Lam <andrewlam2193@gmail.com>
2022-11-29 22:49:42 -0800

    added word bank

--- hangman.asm
+++ hangman.asm
@@ -2,3 +2,18 @@
 # Course: CS2640.01
 # Group: Andrew Lam, Tyler Hipolito, Ben Le, James Contreras 
 # Objective: Create a game of Hangman in MIPS assembly langauge. Utilize all that we have learned in CS2640. 
+
+.data 
+##### Word Bank #####
+word0:	.asciiz "assembly"
+word1:  .asciiz "programming"
+word2:	.asciiz "address"
+word3:  .asciiz "register"
+word4:	.asciiz "memory"
+word5:	.asciiz "intructions"
+word6:  .asciiz "microprocessor"
+word7:	.asciiz "interlocked"
+word8:  .asciiz "pipeline"
+word9:	.asciiz "storage"
+word10:  .asciiz "architecture"
+
